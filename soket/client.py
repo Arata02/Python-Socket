3604fbf89d98f47d971dadc3135fcfc33c8c88f7
Arata02 <arata02.developer@gmail.com>
2020-12-01 09:47:32 +0900

    2st commit

--- soket/client.py
+++ soket/client.py
@@ -5,6 +5,7 @@ import threading
 import re
 import sys
 import struct
+from typing import Optional
 
 #IPアドレス
 host = "127.0.0.1"
@@ -16,18 +17,34 @@ endflag = 1
 
 #数字をサーバに送信する
 def game_start(s):
-    print("ゲームが開始されました.")
+    print("ゲームが開始されました.\n")
     #サーバからの受信用スレッドを作成
     handle_thread = threading.Thread(target=handler, args=(s,), daemon=True)
     handle_thread.start()
     while  endflag > 0:
-        print("数字を入力してください.")
-        number = input()
-        # #送信用データをリトルエンディアンでbyte型 に変換
+
+        number = 0
+        exp_num = 0
+
+        input_num = input().split()
+
+        x = len(input_num)
+        
+        if x == 1:
+            number = input_num[0]
+        elif x == 2:
+            number = input_num[0]
+            exp_num = input_num[1]
+
+        # print(input_num)
+
+        # #送信用データをbyte型 に変換
         #w, x, y, z, a, bの順番でパックする
-        data = struct.pack("BBBBBB", int(number),0,0,0,0,0)
-        #送信
-        print(data)
+        data = struct.pack("BBBBBB", int(number),int(exp_num),0,0,0,0)
+        
+        # print(data)
+
+        # 送信
         s.send(data)
         print('”{}”を送信しました.\n'.format(number))
 
@@ -35,21 +52,28 @@ def game_start(s):
     handle_thread.join()
 
 
-def end_game(point):
+def end_game(point, other1, other2, other3, other4):
     global endflag
     endflag = 0
-    print("ゲームが終了しました.")  
-    print("あなたの得点は{}です.".format(point))
+    print("\nゲームが終了しました.")  
+    print("あなたの得点は{}です.\n".format(point))
+
+    print("その他のクライアントの得点.")
+    print("得点{}".format(other1))
+    print("得点{}".format(other2))
+    print("得点{}".format(other3))
+    print("得点{}".format(other4))
 
     #ゲームが終了したら切断する
-    remove_conection(s)
+    remove_conection(endflag)
 
 
-def remove_conection(s):
-    print("[切断] 通信が切断されました.")
-    print("Enterを押すと終了します.")
-    s.close()
-    sys.exit()
+def remove_conection(endflag):
+    if endflag == 0:
+        print("[切断] 通信が切断されました.")
+        print("Enterを押すと終了します.")
+        s.close()
+        sys.exit()
 
 
 #サーバからメッセージを受信
@@ -58,23 +82,32 @@ def handler(s):
         data = s.recv(6)
         recvdata = struct.unpack("BBBBBB", data)
 
-        print(recvdata)
+        #print(recvdata)
 
         if recvdata[0] == 1:#判定を受け取ったら
             point = recvdata[1]
-            print("貴方の得点{}".format(point))
-            # print("その他を引きました.")
-            # print("得点")
-            #自身以外の判定の時
-            # elif userid != anyuserid:
-                    # print("[受信] 、ほかのユーザが”{}”と入力しました.".format(number))
+            print("\n得点{}\n".format(point))
+
+            print("その他のクライアントの得点.")
+            other1 = recvdata[2]
+            print("得点{}".format(other1))
+            other2 = recvdata[3]
+            print("得点{}".format(other2))
+            other3 = recvdata[4]
+            print("得点{}".format(other3))
+            other4 = recvdata[5]
+            print("得点{}".format(other4))
 
         #ゲーム終了が送られてき時
         if recvdata[0] == 128:
             point = recvdata[1]
-           
+            other1 = recvdata[2]
+            other2 = recvdata[3]
+            other3 = recvdata[4]
+            other4 = recvdata[5]
+
             break
-    end_game(point)
+    end_game(point, other1, other2, other3, other4)
 
 
 if __name__ == "__main__":
--- soket/server.py
+++ soket/server.py
@@ -13,13 +13,16 @@ host = "127.0.0.1"
 port = 8080
 
 # 接続最大数
-maxclient = 1
+maxclient = 5
 
 # クライアント情報
 clients = []
 
 # ポイント
-point = []
+point = [0] * 5
+
+# ユーザそれぞれが拡張機能を送信したかを確認する
+exp_count = [0] * 5
 
 # 終了判定に用いるユーザ
 user_judg = 0
@@ -30,8 +33,8 @@ user_counter = []
 # 終了する受信回数
 end_counter = 3
 
-# 0~255に収めるチェック変数
-#checked = 0
+# 拡張機能のOn・Off
+exp_Flag = False
 
 # アタリとハズレの設定
 hit = random.randint(0, 99)
@@ -47,7 +50,7 @@ elif hit == 0:
 # クライアントと接続を切る
 def remove_conection(con, address):
     userid = clients.index((con, address))
-    print("[切断]ユーザID{}".format(userid))
+    print("[切断] ユーザID{}".format(userid))
     con.close()
     clients.remove((con, address))
 
@@ -74,10 +77,11 @@ def server_start():
             global user_counter
             user_counter.append(0)
             userid = clients.index((con, address))
-            print("[接続]ユーザID{}".format(userid))
+            print("[接続] ユーザID{}".format(userid))
+            
             # ユーザーごとのポイント
-            global point
-            point.append(0)
+            # global point
+            # point.append(0)
 
         game_start()
 
@@ -87,52 +91,44 @@ def handler(con, address):
     # ユーザIDの取得
     userid = clients.index((con, address))
     # 特定のユーザがendcounter回回答を送信したら終了する
-    while user_counter[user_judg-1] < end_counter:
+    while user_counter[user_judg] < end_counter:
         try:
             # データを受け取る最大6byte
             data = con.recv(6)
             recvdata = struct.unpack("<BBBBBB", data)
 
-            print("[受信]ユーザID{} が”{}”と入力しました。".format(userid, recvdata[0]))
+            print("[受信] ユーザID{} が”{}”と入力しました。".format(userid, recvdata[0]))
+            print("拡張機能の入力: {}".format(recvdata[1]))
+
+            # 入力された数値
+            exp = 0
+            exp = int(recvdata[1])
+            if exp == 1:
+                exp_count[userid] = 1
+        
 
             if recvdata[0] == hit:  # 当たりの時
                 point[userid] += 10
-                #check = point[userid]
-                #if check > 255:
-                    #check = 255
-                #elif check < 0:
-                    #check = 0
                 x = judgment(point[userid])
                 point[userid] = x
-                print('ユーザID{} がアタリを引きました. ユーザID{}のポイント= {}'.format(
+                print('ユーザID{} がアタリを引きました. ユーザID{}の得点= {}'.format(
                     userid, userid, point[userid]))
             elif recvdata[0] == blank:  # ハズレの時
                 point[userid] -= 10
-                # check = point[userid]
-                # if check > 255:
-                #     check = 255
-                # elif check < 0:
-                #     check = 0
-                # point[userid] = check
                 x = judgment(point[userid])
                 point[userid] = x
-                print('ユーザID{} がハズレを引きました. ユーザID{}のポイント= {}'.format(
+                print('ユーザID{} がハズレを引きました. ユーザID{}の得点= {}'.format(
                     userid, userid, point[userid]))
-            elif recvdata[0] != hit and recvdata[5] != blank:
+            elif recvdata[0] != hit and recvdata[5] != blank:  # その他の時
                 point[userid] -= 1
-                # check = point[userid]
-                # if check > 255:
-                #     check = 255
-                # elif check < 0:
-                #     check = 0
-                # point[userid] = check
                 x = judgment(point[userid])
                 point[userid] = x
-                print("ユーザID{} がその他を引きました. ユーザID{}のポイント = {}".format(
+                print("ユーザID{} がその他を引きました. ユーザID{}の得点 = {}".format(
                     userid, userid, point[userid]))
 
             for c in clients:
-                c[0].sendto(get_senddata(con, address, 1, recvdata[0]), c[1])
+                # c[0].sendto(get_senddata(con, address, 1, recvdata[0]), c[1])
+                c[0].sendto(get_senddata(c[0], c[1], 1, recvdata[0]), c[1])
             user_counter[userid] += 1
 
         except ConnectionResetError:
@@ -162,8 +158,10 @@ def game_start():
     client_num = len(clients)
     # 終了判定に用いるユーザをランダム選択
     global user_judg
-    user_judg = random.randint(1, client_num)
+    user_judg = random.randint(0, client_num-1)
     print("終了判定はユーザID{}で行います.\n".format(user_judg))
+
+    # print(point)
 
     for c in clients:
         # スレッド処理開始
@@ -171,27 +169,47 @@ def game_start():
                                          args=(c[0], c[1]),
                                          daemon=True)
         handle_thread.start()
+
+        # print(c)
+        
         c[0].sendto(get_senddata(c[0], c[1], 0, 0), c[1])
+        # c[0].sendto(b'get_senddata(c[0], c[1], 0, 0)')
 
     handle_thread.join()
 
 # ゲーム終了
 def end_game():
+    global clients
+    global exp_Flag
+    global exp_count
+
+    if exp_Flag == True:
+        if exp_count[0] == 1:
+            point[0] = 255
+        if exp_count[1] == 1:
+            point[1] = 255
+        if exp_count[2] == 1:
+            point[2] = 255
+        if exp_count[3] == 1:
+            point[3] = 255
+        if exp_count[4] == 1:
+            point[4] = 255
+
     print("ゲームを終了します.")
+
     for c in clients:  # クライアントに終了を伝える
         c[0].sendto(get_senddata(c[0], c[1], 128, 0), c[1])
+    
 
 
-# 送信データを返す
-# 判定時のみnumberを使用。それ以外の時は無視する。
+
+# 送信データを返す。判定時のみnumberを使用。それ以外の時は無視する。
 def get_senddata(con, address, w_type, number):
     # ユーザIDを取得
     userid = clients.index((con, address))
-    # クライアント数を取得
-    clientnumber = len(clients)
-    number = int(number)
-    p = point[userid]
+    g = number
 
+    p = point[userid]
     w = w_type
     x = 0
     y = 0
@@ -200,7 +218,6 @@ def get_senddata(con, address, w_type, number):
     b = 0
 
     if w_type == 0:  # ゲーム開始
-        print("開始")
         x = 0
         y = 0
         z = 0
@@ -208,25 +225,87 @@ def get_senddata(con, address, w_type, number):
         b = 0
 
     elif w_type == 1:  # 判定
-        print("判定")
-        x = p
-        y = 0
-        z = 0
-        a = 0
-        b = 0
-
-    elif w_type == 128:  # ゲーム終了
-        print("終了")
-        x = p
-        y = 0
-        z = 0
-        a = 0
-        b = 0
-
+        if userid == 0:
+            x = p
+            y = point[1]
+            z = point[2]
+            a = point[3]
+            b = point[4]
+        elif userid == 1:
+            x = p
+            y = point[0]
+            z = point[2]
+            a = point[3]
+            b = point[4]
+        elif userid == 2:
+            x = p
+            y = point[0]
+            z = point[1]
+            a = point[3]
+            b = point[4]
+        elif userid == 3:
+            x = p
+            y = point[0]
+            z = point[1]
+            a = point[2]
+            b = point[4]
+        elif userid == 4:
+            x = p
+            y = point[0]
+            z = point[1]
+            a = point[2]
+            b = point[3]
+    elif w_type == 128:  # 終了
+        if userid == 0:
+            x = p
+            y = point[1]
+            z = point[2]
+            a = point[3]
+            b = point[4]
+        elif userid == 1:
+            x = p
+            y = point[0]
+            z = point[2]
+            a = point[3]
+            b = point[4]
+        elif userid == 2:
+            x = p
+            y = point[0]
+            z = point[1]
+            a = point[3]
+            b = point[4]
+        elif userid == 3:
+            x = p
+            y = point[0]
+            z = point[1]
+            a = point[2]
+            b = point[4]
+        elif userid == 4:
+            x = p
+            y = point[0]
+            z = point[1]
+            a = point[2]
+            b = point[3]
+        
     data = struct.pack("<BBBBBB", w, x, y, z, a, b)
     return data
 
+
 if __name__ == "__main__":
     print("アタリ：{}, ハズレ：{}".format(hit, blank))
+    print("拡張機能をOnにしますか?\n0 (On) or 1 (Off)")
+    num = int(input())
+    if num == 0:
+        print("On\n")
+        exp_Flag = True
+    elif num == 1:
+        print("Off\n")
+        exp_Flag = False
+    else:
+        print("その他が入力されました.\n拡張機能はOffです.\n")
+        exp_Flag = False
+
     server_start()
-    print("ゲームを終了します.")
+
+    socket.close
+    sys.exit
